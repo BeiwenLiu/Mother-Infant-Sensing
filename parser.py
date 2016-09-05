9040ce79b7b460520513df60fcdeda2099065f53
Beiwen Liu <bliu85@gatech.edu>
2016-09-05 12:23:43 -0400

    Span time across empty actions

--- parser.py
+++ parser.py
@@ -17,14 +17,16 @@ def multipleParse():
     
     for file in files:
         parse(file)
-
+        
+#identifies time range and action from HTML table
 def parse(fileName):
     df = pd.read_html(fileName)
     number = len(df[0][1])
+    increment = timedelta(microseconds=100000)
     tierNames = findTiers(fileName)
-    startFrame, time = findBeginning(fileName)
-    print startFrame, time
+    
     for element in tierNames:
+        startFrame, time = findBeginning(fileName)
         sa = pd.DataFrame(columns=['Time','Action'])
         for x in range(0,number,4):
             if element == df[0][1][x]:
@@ -33,14 +35,17 @@ def parse(fileName):
                 timestamp = df[0][1][x+2]
                 start,end = timestamp.split(' - ')
                 print 'Initializing conversion'
-                tempDf = createDF(start,end,timedelta(microseconds=100000),action)
-                sa=sa.append(tempDf)
+                firstDf = createDF(time, start,increment)
+                tempDf = createDF(start,end,increment,action)
+                sa = pd.concat([sa,firstDf],ignore_index=True)
+                sa = pd.concat([sa,tempDf],ignore_index=True)
+                time = end
         print "element:", element
         print sa
         sa.to_csv("{}.csv".format(element))
     
 #Creates dataframe and spans time across interval
-def createDF(start,end,delta,action):
+def createDF(start,end,delta,action = ""):
     unixT = unix('2056-05-05','00:53:12')
     print unixT
     start = stringToTime(start)
@@ -57,6 +62,7 @@ def createDF(start,end,delta,action):
     df['Time'] = a
     df['Action'] = action
     return df
+    
 
 #Rounds to nearest tenth of second. Input must be of type dateTime
 def roundTime(time):
@@ -95,6 +101,7 @@ def findTiers(filename):
             a.append(tempElement)
     return a
 
+#finds Start and its time for offset
 def findBeginning(filename):
     temp = pd.read_html(filename)
     number = len(temp[0][1])
@@ -105,13 +112,12 @@ def findBeginning(filename):
             start,end = timestamp.split(' - ')
             start = stringToTime(start)
             start = roundTime(start)
+            start = stringConverter(start)
             df = pd.DataFrame(columns=['Time','Action'])
-            df['Time'] = [stringConverter(start)]
+            df['Time'] = [start]
             df['Action'] = [tempElement]
-            print df
-            return df
-            
-findBeginning('tiertesting.html')           
+            return df, start
+                      
 
  
 def unix(dateStamp,timeStamp):
@@ -125,3 +131,6 @@ def unix(dateStamp,timeStamp):
 #Add rounding
 #improve unix timestamp
 #export to csv
+#finds start time
+
+multipleParse()
