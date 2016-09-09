57d0a578684fa28783b91791d1a44934ef796da5
Beiwen Liu <beiwenl@gmail.com>
2016-09-08 22:42:07 -0400

    allow toggle of offset function

--- ElanDataFormatting/parser.py
+++ ElanDataFormatting/parser.py
@@ -13,42 +13,100 @@ import time
 import calendar
 
 #specify files here
-def multipleParse():
-    files = ['html/tierTesting.html']
-    
-    for file in files:
-        parse(file)
+#True or False if you have created a Start tier
+
+#if True, will create offset, Unix timestamp, and propagate empty actions
+#if False, will only create time intervals where annotation exists
+def multipleParse(files,offset):
+    if offset == True:
+        for file in files:
+            parseUnix(file)
+    else:
+        for file in files:
+            parse(file)
         
-#identifies time range and action from HTML table
+#creates time range and action from HTML table
 def parse(fileName):
 #specify Date and Time at Start Here    
     unixTime = unix('00:00:00.0','1970-01-01')
     df = pd.read_html(fileName)
     number = len(df[0][1])
+    print df[0][1]
     increment = timedelta(microseconds=100000)
     tierNames = findTiers(fileName)
-    startFrame, timeOffset = findBeginning(fileName)
+    
+    #startFrame, timeOffset = findBeginning(fileName)
+    timeOffset = "00:00:00.0"
     for element in tierNames:
-        startFrame, time = findBeginning(fileName)
+        #startFrame, time = findBeginning(fileName)
         sa = pd.DataFrame(columns=['Time','Action'])
-        for x in range(0,number,4):
+        x = 0
+        while x < number:
             if element == df[0][1][x]:
                 print "tier:", df[0][1][x]
                 action = df[0][1][x + 1]
                 timestamp = df[0][1][x+2]
+                print "action:",action
+                print"timestamp:",timestamp
+                print"S1",df[0][1][x+3]
                 start,end = timestamp.split(' - ')
                 print 'Initializing conversion'
-                firstDf = createDF(time, start,increment,timeOffset,unixTime)
-                tempDf = createDF(start,end,increment,timeOffset,unixTime,action)
+                #firstDf = createDF(time, start,increment,unixTime,timeOffset)
+                tempDf = createDF(start,end,increment,unixTime,timeOffset,action)
+                #sa = pd.concat([sa,firstDf],ignore_index=True)
+                sa = pd.concat([sa,tempDf],ignore_index=True)
+                #time = end
+                x += 4
+            elif df[0][1][x] == "TC":
+                x += 3
+            else:
+                x += 4
+                    
+        print "element:", element
+        print "SA",sa
+        sa.to_csv("csv/{}.csv".format(element))
+        
+def parseUnix(fileName):
+#specify Date and Time at Start Here    
+    unixTime = unix('00:00:00.0','1970-01-01')
+    df = pd.read_html(fileName)
+    number = len(df[0][1])
+    print df[0][1]
+    increment = timedelta(microseconds=100000)
+    tierNames = findTiers(fileName)
+    
+    startFrame, timeOffset = findBeginning(fileName)
+    for element in tierNames:
+        startFrame, time = findBeginning(fileName)
+        sa = pd.DataFrame(columns=['Time','Action'])
+        x = 0
+        while x < number:
+            if element == df[0][1][x]:
+                print "tier:", df[0][1][x]
+                action = df[0][1][x + 1]
+                timestamp = df[0][1][x+2]
+                print "action:",action
+                print"timestamp:",timestamp
+                print"S1",df[0][1][x+3]
+                start,end = timestamp.split(' - ')
+                print 'Initializing conversion'
+                firstDf = createDFUnix(time, start,increment,unixTime,timeOffset)
+                tempDf = createDFUnix(start,end,increment,unixTime,timeOffset,action)
                 sa = pd.concat([sa,firstDf],ignore_index=True)
                 sa = pd.concat([sa,tempDf],ignore_index=True)
                 time = end
+                x += 4
+            elif df[0][1][x] == "TC":
+                x += 3
+            else:
+                x += 4
+                    
         print "element:", element
         print sa
         sa.to_csv("csv/{}.csv".format(element))
     
 #Creates dataframe and spans time across interval
-def createDF(start,end,delta,offset,unixTime,action = ""):
+def createDF(start,end,delta,unixTime,offset,action = ""):
     start = stringToTime(start)
     end = stringToTime(end)
     start = roundTime(start)
@@ -62,6 +120,27 @@ def createDF(start,end,delta,offset,unixTime,action = ""):
         temp = current - offsetSeconds
         #a.append(unix(stringConverter(temp))+unixTime) 
         a.append(stringConverter(current))
+        #stringConverter(current) for time representation
+        current += delta
+    
+    df['Time'] = a
+    df['Action'] = action
+    return df
+    
+def createDFUnix(start,end,delta,unixTime,offset,action = ""):
+    start = stringToTime(start)
+    end = stringToTime(end)
+    start = roundTime(start)
+    end = roundTime(end)
+    current = start
+    df = pd.DataFrame(columns=['Time','Action'])
+    a = []
+    offsetSeconds = computeSecondOffset(offset)
+    offsetSeconds = timedelta(seconds = offsetSeconds)
+    while current < end:
+        temp = current - offsetSeconds
+        a.append(unix(stringConverter(temp))+unixTime) 
+        #a.append(stringConverter(current))
         #stringConverter(current) for time representation
         current += delta
     
@@ -95,16 +174,21 @@ def findTiers(filename):
     temp = pd.read_html(filename)
     number = len(temp[0][1])
     a = []
-    for x in range(0,number,4):
+    x = 0
+    while x < number:
         tempElement = temp[0][1][x]
         answer = False
-        if tempElement == "Start":
+        if tempElement == "Start" or tempElement == "TC":
             answer = True
         for element in a:
             if element == tempElement:
                 answer = True
         if answer == False:
             a.append(tempElement)
+        if tempElement == "TC":
+            x += 3
+        else:
+            x += 4
     return a
 
 #finds Start and its time for offset
@@ -142,4 +226,4 @@ def unix(timeStamp,dateStamp="1970-01-01"):
     timeUnix2 = float(timeStamp[-2:])
     return dateUnix+timeUnix+timeUnix2
 
-multipleParse()
+multipleParse(['html/tiertesting.html'],False)
